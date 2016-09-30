c48af0306e6522d38647dc72b1304ac8656016d1
Natalja-Olefire <natalja.olefire@gmail.com>
2016-09-30 19:24:45 -0300

    ngTouch added to the app module

--- scripts/words.js
+++ scripts/words.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 
-angular.module('Words', [])
+angular.module('Words', ['ngTouch'])
 .controller('WordsController', WordsController)
 .service('WordsService', WordsService);
 
